5a0bd2822bd482dcc66687dada05d7e570f47468
Ahmadfauzi34 <89245707+Ahmadfauzi34@users.noreply.github.com>
2026-05-02 00:57:37 +0700

    Update clippy lints in lib.rs
    Removed some clippy lints and added new ones with comments for future refactoring.

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,22 @@
         clippy::pedantic,
         clippy::nursery,
         clippy::cargo,
-        clippy::perf,
-        clippy::complexity,
-        clippy::style,
     )
 )]
 #![allow(
     clippy::uninlined_format_args,
-    clippy::missing_const_for_fn,
-    clippy::suboptimal_flops,
     clippy::similar_names,
     clippy::explicit_iter_loop,
+    clippy::missing_const_for_fn,
+    // TODO: Hapus allow ini jika refactoring dasar sudah selesai untuk optimasi FMA
+    clippy::suboptimal_flops,
+    // Linter Kerapian & Dokumentasi
+    clippy::unused_self,
+    clippy::use_self,
+    clippy::doc_markdown,
+    clippy::missing_panics_doc,
 )]
-#![cfg_attr(not(test), deny(clippy::correctness, clippy::suspicious,))]
+#![cfg_attr(not(test), deny(clippy::correctness, clippy::suspicious))]
 #![allow(
     clippy::module_name_repetitions,
     clippy::must_use_candidate,
@@ -29,6 +32,7 @@
     clippy::cast_precision_loss,
     clippy::cast_sign_loss,
 )]
+
 
 pub mod core;
 pub mod memory;
